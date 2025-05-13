59e1cdaaf029ba6928536d32103f87c51bcdbc0a
Martin Bonnin <martin@mbonnin.net>
2025-05-13 14:04:45 +0200

    fix coordinates of the included build

--- test-plugin/build.gradle.kts
+++ test-plugin/build.gradle.kts
@@ -5,7 +5,7 @@ buildscript {
         maven("https://storage.googleapis.com/gradleup/m2")
     }
     dependencies {
-        classpath("build-logic:build-logic")
+        classpath("gratatouille-build-logic:gratatouille-build-logic")
         classpath(libs.ksp.gradle.plugin)
         classpath("com.gradleup.gratatouille:gratatouille-gradle-plugin")
     }
