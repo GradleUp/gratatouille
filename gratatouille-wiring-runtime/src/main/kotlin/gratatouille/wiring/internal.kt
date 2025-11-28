7cea18d40787429e843fc1a96e328bd289776a4e
Martin Bonnin <martin@mbonnin.net>
2025-11-28 19:40:34 +0100

    Remove json dependency

--- gratatouille-wiring-runtime/src/main/kotlin/gratatouille/wiring/internal.kt
+++ gratatouille-wiring-runtime/src/main/kotlin/gratatouille/wiring/internal.kt
@@ -1,29 +1,11 @@
 package gratatouille.wiring
 
-import kotlinx.serialization.encodeToString
-import kotlinx.serialization.json.Json
 import org.gradle.api.file.FileCollection
-import java.io.File
 import java.lang.reflect.InvocationTargetException
 import java.lang.reflect.Method
 
 @RequiresOptIn(message = "This symbol is only to be used by Gratatouille generated wiring code")
 annotation class GratatouilleWiringInternal
-
-@PublishedApi
-internal val json = Json {
-  classDiscriminator = "#class"
-}
-
-@GratatouilleWiringInternal
-inline fun <reified T> T.encodeJson(): String {
-  return json.encodeToString(this)
-}
-
-@GratatouilleWiringInternal
-inline fun <reified T> T.encodeJsonTo(file: File) {
-  return file.writeText(json.encodeToString(this))
-}
 
 @GratatouilleWiringInternal
 fun FileCollection.isolateFileCollection(): List<Any> = buildList {
