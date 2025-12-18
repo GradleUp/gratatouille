ed93feed42d14398a06a3a9bc93ec396a42b06b2
Martin Bonnin <martin@mbonnin.net>
2025-12-18 11:05:41 +0100

    Update KGP, Gratatouille, Librarian

--- gratatouille-build-logic/src/main/kotlin/main.kt
+++ gratatouille-build-logic/src/main/kotlin/main.kt
@@ -1,6 +1,8 @@
 import com.gradleup.librarian.gradle.Librarian
 import org.gradle.api.Project
 import org.gradle.api.publish.PublishingExtension
+import org.jetbrains.kotlin.gradle.dsl.KotlinJvmProjectExtension
+import org.jetbrains.kotlin.gradle.dsl.KotlinProjectExtension
 
 fun Project.module() {
   Librarian.module(this)
@@ -11,6 +13,12 @@ fun Project.module() {
     it.maven {
       it.name = "PluginTest"
       it.url = uri(rootDir.resolve("build/m2"))
+    }
+  }
+
+  extensions.findByType(KotlinJvmProjectExtension::class.java)?.apply {
+    compilerOptions {
+        freeCompilerArgs.add("-Xsuppress-version-warnings")
     }
   }
 }
