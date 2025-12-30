38edda6d29a05d42a08d27aa1667f70de54a35ef
Martin Bonnin <martin@mbonnin.net>
2025-12-30 13:37:05 +0100

    Update librarian-cli dependency version

--- scripts/update-repo.main.kts
+++ scripts/update-repo.main.kts
@@ -1,9 +1,11 @@
 #!/usr/bin/env kotlin
 
 @file:Repository("https://repo.maven.apache.org/maven2/")
+@file:Repository("https://dl.google.com/android/maven2/")
 @file:Repository("https://storage.googleapis.com/gradleup/m2")
 @file:Repository("https://jitpack.io")
-@file:DependsOn("com.gradleup.librarian:librarian-cli:0.1.1-SNAPSHOT-44a36034d83f8ba9c9177653f29df3dea933fc3f")
+//@file:Repository("file://~/.m2/repository")
+@file:DependsOn("com.gradleup.librarian:librarian-cli:0.2.2-SNAPSHOT-0cbca80f60a15a0f851a9cd468bfc352db316dd4")
 
 import com.gradleup.librarian.cli.updateRepo
 
